de453f43419619c28529a0b70742a705d1b9d8d4
gre <18122436634@163.com>
2025-09-28 22:23:22 +0800

    云平台开发 1、数据库表设计 2、初始化前后端项目 3、后端代码生成器功能开发 4、前端管理页面和主页开发

---
+++ code-generator-web-backend/src/main/java/com/gre/web/mapper/GeneratorMapper.java
@@ -0,0 +1,18 @@
+package com.gre.web.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.gre.web.model.entity.Generator;
+
+/**
+ * @author Administrator
+ * @description 针对表【generator(代码生成器)】的数据库操作Mapper
+ * @createDate 2025-09-24 21:33:35
+ * @Entity com.gre.web.model.entity.Generator
+ */
+public interface GeneratorMapper extends BaseMapper<Generator> {
+
+}
+
+
+
+
